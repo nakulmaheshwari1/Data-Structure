20b7e3f1f9d83f41a0fee456e5aed3c9d09607c6
Nakul Maheshwari <maheshwarinakul04@gmail.com>
2025-01-15 19:06:50 +0530

    delete and searching

--- array.c
+++ array.c
@@ -15,6 +15,7 @@ int main()
     for(i=0;i<n;i++){
         printf("|%d|",a[i]);
     }
+    printf("\n");
     // insertion at start (index==0)
     for(i=n-1;i>=0;i--){
         a[i+1]=a[i];
@@ -26,9 +27,56 @@ int main()
     for(i=0;i<n+1;i++){
         printf("|%d|",a[i]);
     }
-
-
-
+    printf("\n");
+    // insertion at last
+    printf("Enter the element to insert at last:");
+    scanf("%d",&a[i]);
+    //display the array
+    for(i=0;i<=n+1;i++){
+        printf("|%d|",a[i]);
+    }
+    printf("\n");
+    // insertion at m-th index
+    int m;
+    printf("Enter index where you want to enter the element:");
+    scanf("%d",&m);
+    for(i=n+1;i>=m;i--){
+        a[i+1]=a[i];
+    }
+    printf("Enter element to be enter at m-th index:");
+    scanf("%d",&a[m]);
+    for(i=0;i<n+3;i++){
+        printf("|%d|",a[i]);
+    }
+    printf("\n");
+    //deleting an element
+    int d;
+    printf("Enter the index of element want to delete:");
+    scanf("%d",&d);
+    for(i=d;i<n+3;i++){
+        a[i]=a[i+1];
+    }
+    for(i=0;i<n+2;i++){
+        printf("|%d|",a[i]);
+    }
+    printf("\n");
+    //searching an element(linear search)
+    int s,count=0;
+    printf("Enter the element to be search:");
+    scanf("%d",&s);
+    for(i=0;i<n+2;i++){
+        if(a[i]==s){
+        count ++;
+        }
+    }
+    if(count!=0){
+        printf("%d appear %d times",s,count);
+    }else{
+        printf("Element not found");
+    }
+    printf("\n");
+    // sorting
+   
 
     return 0;
 }
