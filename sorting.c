8bbfa13a7cb324b7f56434d5954299dae59070d7
Nakul Maheshwari <maheshwarinakul04@gmail.com>
2025-01-17 08:53:54 +0530

    Merge Sorting

--- sorting.c
+++ sorting.c
@@ -1,6 +1,65 @@
-#include<stdio.h>
+#include <stdio.h>
+void mergesort(int a[], int st, int mid, int end)
+{
+    int temp[100];
+    int i = st, j = mid + 1, k = st;
+    while (i <= mid && j <= end)
+    {
+        if (a[i] <= a[j])
+        {
+            temp[k] = a[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            temp[k] = a[j];
+            j++;
+            k++;
+        }
+    }
+    while (i <= mid)
+    {
+        temp[k] = a[i];
+        i++;
+        k++;
+    }
+    while (j <= end)
+    {
+        temp[k] = a[j];
+        j++;
+        k++;
+    }
+    for (i = st; i <= end; i++)
+    {
+        a[i] = temp[i];
+    }
+    
+}
+int divide(int a[], int st, int end)
+{
+    int mid = st + (end - st) / 2;
+    if (st < end)
+    {
+        divide(a, st, mid);
+        divide(a, mid + 1, end);
+        mergesort(a, st, mid, end);
+    }
+}
+
 int main()
 {
-    
+    int a[10];
+    int i;
+    printf("Enter the element:");
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    divide(a, 0, 9);
+    for (i = 0; i < 10; i++)
+    {
+        printf("|%d|", a[i]);
+    }
     return 0;
 }
