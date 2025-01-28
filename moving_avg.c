b88f3ec81dc78f90f06c1aaae25176f90eb1240f
Nakul Maheshwari <maheshwarinakul04@gmail.com>
2025-01-28 22:39:29 +0530

    exponential moving average

--- moving_avg.c
+++ moving_avg.c
@@ -24,7 +24,23 @@ void simple_Moving_Avg(int a[],int n, int m ){
     }
 }
 
-
+// exponential moving average
+void Exp_Mov_avg(int arr[], int n, int size_win) {
+    int B[n - size_win + 1];
+    float EMA = arr[0]; 
+    float a = 2.0 / (size_win + 1); 
+    
+    for (int i = 1; i < n; i++) {
+        EMA = (arr[i] * a) + EMA * (1 - a);
+        if (i >= size_win - 1) {
+            B[i - size_win + 1] = EMA;
+        }
+    }
+    for (int i = 0; i<n-size_win+1; i++) {
+        printf("%d ", B[i]);
+    }
+    printf("\n");
+}
 
 int main()
 {
@@ -41,6 +57,20 @@ int main()
     printf("Enter the size of moving avg window:");
     scanf("%d", &m);
     simple_Moving_Avg(a,n,m);
+
+
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        printf("Enter the element: ");
+        scanf("%d", &arr[i]);
+    }
+    int size_win;
+    printf("Enter the size of the window: ");
+    scanf("%d", &size_win);
+    Exp_Mov_avg(arr, n, size_win);
 
     return 0;
 }
