11b32b5af210fa2de88c9ed31770fb6e3d313358
Nakul Maheshwari <maheshwarinakul04@gmail.com>
2025-02-19 08:30:14 +0530

    changes

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,57 @@
 #include <stdlib.h>
 
 struct node
-    {
-        int data;
-        struct node *next;
-    };
+{
+    int data;
+    struct node *next;
+};
 
-void printlist( struct node *head){
+void printlist(struct node *head)
+{
     struct node *curr = head;
-    while(curr !=NULL){
-        printf("|%d|",curr->data);
+    while (curr != NULL)
+    {
+        printf("|%d|", curr->data);
         curr = curr->next;
     }
+}
+
+void insert_at_Beginning(struct node *head, int data)
+{
+    struct node *new_node;
+    new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->next = head;
+    head = new_node;
+}
+
+void insert_at_specificPosition(struct node *head, int data, int position)
+{
+    struct node *new_node;
+    new_node = (struct node *)malloc(sizeof(struct node));
+    struct node *temp = head;
+    int i;
+    for (i = 1; i < position - 1; i++)
+    {
+        temp = temp->next;
+    }
+    new_node->data = data;
+    new_node->next = temp->next;
+    temp->next = new_node;
+}
+
+void insert_at_end(struct node *head, int data)
+{
+    struct node *new_node;
+    new_node = (struct node *)malloc(sizeof(struct node));
+    struct node *temp = head;
+    while (temp->next != 0)
+    {
+        temp = temp->next;
+    }
+    temp->next = new_node;
+    temp = new_node;
+    temp->data = data;
+    temp->next = 0;
 }
 
 int main()
@@ -21,6 +61,8 @@ int main()
     struct node *head, *nnode, *temp;
     head = 0;
     int option = 1;
+    char choice;
+    int pos, data;
     while (option != 0)
     {
         nnode = (struct node *)malloc(sizeof(struct node));
@@ -41,5 +83,14 @@ int main()
         scanf("%d", &option);
     }
     printlist(head);
+    printf("\n");
+    printf("Do you want to enter the data\n");
+    printf("Y for yes \nN for no");
+    getchar();           // when we shift scanf from int to char input data type the new line character because of enter key we press remain in the input buffer nd if we not use getchar the new line will read by scanf and cause error 
+    scanf("%c", &choice);
+    if (choice == 'Y')
+    {
+        printf("Enter position where you want to enter data:");
+    }
     return 0;
 }
